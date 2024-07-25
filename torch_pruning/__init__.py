c419ddb61c07f7e39d35dcba518aff32cc95b5f1
zkl-ai <907668776@qq.com>
2024-07-25 12:55:06 +0000

    hhhhh

--- torch_pruning/__init__.py
+++
@@ -1,6 +0,0 @@
-from .pruner import importance
-from .dependency import *
-from .pruner import *
-from . import _helpers, utils
-
-from .serialization import save, load, state_dict, load_state_dict
--- torch_pruning/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import *
-from .op_counter import count_ops_and_params
-from . import benchmark
