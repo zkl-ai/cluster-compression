1358002b07db908de099fed4f00a0a7c20aba0a9
zkl-ai <907668776@qq.com>
2024-07-25 13:42:18 +0000

    hhhh

--- benchmarks/perf_measure.py
+++ benchmarks/perf_measure.py
@@ -14,6 +14,22 @@ import torchvision
 import torch_pruning as tp 
 ip_address, hostname = get_host_ip()
 
+def split_dict(pruning_ratio_dict):
+    total_items = len(pruning_ratio_dict)
+    half_items = total_items // 2
+
+    dict1 = {}
+    dict2 = {}
+
+    for i, (key, value) in enumerate(pruning_ratio_dict.items()):
+        if i < half_items:
+            dict1[key] = value
+        else:
+            dict2[key] = value
+    
+    return dict1, dict2
+
+
 def get_args_parser(add_help=True):
     import argparse
 
@@ -66,8 +82,10 @@ def runner_imagenet(model_type, prune_ratios):
                     pruning_ratio_dict[m] = prune_ratios[pruning_ratio_idx]
                     pruning_ratio_idx += 1
 
+    
+    pruning_ratio_dict1, pruning_ratio_dict2 = split_dict(pruning_ratio_dict)
     imp = tp.importance.MagnitudeImportance(p=2)
-    pruner = tp.pruner.MetaPruner(
+    pruner1 = tp.pruner.MetaPruner(
         model,
         example_inputs,
         importance=imp,
@@ -77,9 +95,24 @@ def runner_imagenet(model_type, prune_ratios):
     )
     model = model.to('cpu')
     print("="*16)
-    print("After pruning:")
-    pruner.step()
+    print("After pruning 1:")
+    pruner1.step()
     print(model)
+    
+    pruner2 = tp.pruner.MetaPruner(
+        model,
+        example_inputs,
+        importance=imp,
+        pruning_ratio=1.0,
+        pruning_ratio_dict=pruning_ratio_dict,
+        ignored_layers=ignored_layers,
+    )
+    model = model.to('cpu')
+    print("="*16)
+    print("After pruning 2:")
+    pruner2.step()
+    print(model)
+    
     pruned_ops, pruned_size = tp.utils.count_ops_and_params(model, example_inputs=example_inputs)
     print("Params: {:.2f} M => {:.2f} M ({:.2f}%)".format(base_params / 1e6, pruned_size / 1e6, pruned_size / base_params * 100))
     print("Ops: {:.2f} G => {:.2f} G ({:.2f}%, {:.2f}X )".format(base_ops / 1e9, pruned_ops / 1e9, pruned_ops / base_ops * 100, base_ops / pruned_ops))
@@ -89,7 +122,8 @@ def runner_imagenet(model_type, prune_ratios):
     model = model.eval().to(device)
     batch_example_inputs = torch.randn(args.batch_size, 3, 224, 224).to(device)
     with torch.no_grad():
-        latency_mu, latency_std = tp.utils.benchmark.measure_latency(model, batch_example_inputs, repeat=10)
+        # latency_mu, latency_std = tp.utils.benchmark.measure_latency(model, batch_example_inputs, repeat=10)
+        latency_mu, latency_std = 0, 0
         print('latency: {:.4f} +/- {:.4f} ms'.format(latency_mu, latency_std))
     
     del model, example_inputs, batch_example_inputs
